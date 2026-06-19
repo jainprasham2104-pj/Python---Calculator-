085428553e75d6be8445935bfa20db31d8e7b1d0
jainprasham2104-pj <jainprasham2104@gmail.com>
2026-06-19 10:06:14 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,36 @@
+print("===== Prasham's Calculator =====")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("\nChoose Operation")
+print("1. Addition (+)")
+print("2. Subtraction (-)")
+print("3. Multiplication (*)")
+print("4. Division (/)")
+
+choice = input("Enter choice (1/2/3/4): ")
+
+if choice == '1':
+    result = num1 + num2
+    print("\nResult =", result)
+
+elif choice == '2':
+    result = num1 - num2
+    print("\nResult =", result)
+
+elif choice == '3':
+    result = num1 * num2
+    print("\nResult =", result)
+
+elif choice == '4':
+    
+    if num2 != 0:
+        result = num1 / num2
+        print("\nResult =", result)
+    else:
+        print("\nError! Division by zero is not allowed.")
+
+else:
+    print("\nInvalid Input!")
+
